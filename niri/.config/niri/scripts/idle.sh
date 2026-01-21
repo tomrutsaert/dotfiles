37d5b0999dfa91bea6d95e221e2786c20ce62f0a
Tom Rutsaert <tom.rutsaert@inmail.be>
2026-01-21 16:04:09 +0100

    improvements

--- niri/.config/niri/scripts/idle.sh
+++ niri/.config/niri/scripts/idle.sh
@@ -2,8 +2,6 @@
 swayidle -w \
     timeout 300 "~/.config/sway/scripts/manage_vcp.sh save && ~/.config/sway/scripts/manage_vcp.sh dim" \
         resume "~/.config/sway/scripts/manage_vcp.sh restore" \
-    timeout 450 'swaymsg "output * dpms off"' \
-        resume 'swaymsg "output * dpms on"' \
-    timeout 600 'swaymsg "output * power off"' \
-        resume 'swaymsg "output * power on"' \
+    timeout 450 'niri msg output DP-1 off && niri msg output DP-2 off' \
+        resume 'niri msg output DP-1 on && niri msg output DP-2 on' \
     timeout 3600 'systemctl suspend'
